d4e06b4611098b93a4920a00ff212e086bb58df5
felipe.domingues <felipe.domingues@calix.com>
2023-04-22 05:25:16 -0300

    add analytics

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -20,5 +20,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+export const analytics = getAnalytics(app);
 export const database = getFirestore(app);
